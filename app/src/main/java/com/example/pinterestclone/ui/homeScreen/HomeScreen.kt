cee6953e16b9a9ebebc542bd3a8467b4ff6d046a
Ozlem Basabakar <ozlembasabakar@gmail.com>
2023-02-16 17:49:52 +0300

    add bottom bar to the HomeScreen. fix height and padding of the BottomBar.kt. Fix the Shape.kt

---
+++ app/src/main/java/com/example/pinterestclone/ui/homeScreen/HomeScreen.kt
@@ -0,0 +1,31 @@
+package com.example.pinterestclone.ui.homeScreen
+
+import android.content.res.Configuration
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pinterestclone.ui.homeScreen.bottomBar.BottomBar
+import com.example.pinterestclone.ui.theme.PinterestCloneTheme
+
+@Composable
+fun HomeScreen(modifier: Modifier) {
+    Scaffold(
+        bottomBar = {
+            BottomBar(modifier = modifier)
+        },
+        backgroundColor = MaterialTheme.colors.surface
+    ) {
+
+    }
+}
+
+@Preview(name = "LightMode", showSystemUi = true)
+@Preview(name = "DarkMode", uiMode = Configuration.UI_MODE_NIGHT_YES, showSystemUi = true)
+@Composable
+fun HomeScreenPreview() {
+    PinterestCloneTheme {
+        HomeScreen(Modifier)
+    }
+}
--- app/src/main/java/com/example/pinterestclone/ui/homeScreen/bottomBar/BottomBarItem.kt
+++ app/src/main/java/com/example/pinterestclone/ui/homeScreen/bottomBar/BottomBarItem.kt
@@ -13,8 +13,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.pinterestclone.R
+import com.example.pinterestclone.ui.theme.BottomBarItemSize
 import com.example.pinterestclone.ui.theme.PinterestCloneTheme
 
 @Composable
@@ -24,7 +24,7 @@ fun BottomBarItem(
 ) {
     Box(
         modifier = modifier
-            .size(height = 48.dp, width = 48.dp)
+            .size(BottomBarItemSize)
             .fillMaxSize()
             .clip(shape = MaterialTheme.shapes.small)
             .background(MaterialTheme.colors.surface),
