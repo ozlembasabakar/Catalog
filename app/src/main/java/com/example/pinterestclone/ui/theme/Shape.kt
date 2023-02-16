aba0c72fee3f1e5f6d9457f4dfd71498de3862bd
Ozlem Basabakar <ozlembasabakar@gmail.com>
2023-02-16 15:54:12 +0300

    add corner radius, padding values, alignments, width and height. add colors to the Theme file.

--- app/src/main/java/com/example/pinterestclone/ui/theme/Shape.kt
+++ app/src/main/java/com/example/pinterestclone/ui/theme/Shape.kt
@@ -1,11 +1,26 @@
 package com.example.pinterestclone.ui.theme
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
-    small = RoundedCornerShape(4.dp),
+    small = RoundedCornerShape(12.dp),
     medium = RoundedCornerShape(4.dp),
     large = RoundedCornerShape(0.dp)
 )
+
+val BottomBarCornerRadius = Shapes.small
+val BottomBarHorizontalPadding = PaddingValues(12.dp)
+val BottomBarStartPadding = PaddingValues(4.dp)
+val BottomBarEndPadding = PaddingValues(16.dp)
+val BottomBarPaddingBetweenItems = PaddingValues(8.dp)
+val BottomBarItemAlignment = Alignment.Center
+val BottomBarItemsAlignment = Alignment.CenterHorizontally
+val BottomBarHeight = Modifier.height(80.dp)
+val BottomBarWidth = Modifier.fillMaxWidth()
--- app/src/main/java/com/example/pinterestclone/ui/theme/Theme.kt
+++ app/src/main/java/com/example/pinterestclone/ui/theme/Theme.kt
@@ -1,30 +1,25 @@
 package com.example.pinterestclone.ui.theme
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
+@SuppressLint("ConflictingOnColor")
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
     background = Color.White,
     surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
     onSurface = Color.Black,
-    */
+)
+
+@SuppressLint("ConflictingOnColor")
+private val LightColorPalette = lightColors(
+    background = Color.White,
+    surface = Color.White,
+    onSurface = Color.Black,
 )
 
 @Composable
