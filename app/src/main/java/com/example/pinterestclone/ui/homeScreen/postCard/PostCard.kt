e4c182fef1fddcafcf394a3a416592cb462adb63
Ozlem Basabakar <ozlembasabakar@gmail.com>
2023-02-20 11:58:32 +0300

    create PostCard.

---
+++ app/src/main/java/com/example/pinterestclone/ui/homeScreen/postCard/PostCard.kt
@@ -0,0 +1,73 @@
+package com.example.pinterestclone.ui.homeScreen.postCard
+
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.pinterestclone.R
+import com.example.pinterestclone.ui.theme.PinterestCloneTheme
+
+@Composable
+fun PostCard(
+    modifier: Modifier,
+    image: Int,
+) {
+/*    val width = LocalConfiguration.current.run {
+        ((screenWidthDp) / 2).dp
+    }*/
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(4.dp)
+            .clip(MaterialTheme.shapes.small)
+            .background(MaterialTheme.colors.surface)
+    ) {
+        Image(
+            painter = painterResource(id = image),
+            contentDescription = "",
+            modifier = Modifier
+                .fillMaxSize()
+                .clip(MaterialTheme.shapes.small),
+            contentScale = ContentScale.Crop
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 4.dp, horizontal = 8.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.more_icon),
+                contentDescription = "",
+                tint = MaterialTheme.colors.onSurface,
+                modifier = Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Icon(
+                painter = painterResource(id = R.drawable.more_icon),
+                contentDescription = "",
+                tint = MaterialTheme.colors.onSurface,
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
+
+@Preview(name = "LightMode")
+@Preview(name = "DarkMode", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun PostCardPreview() {
+    PinterestCloneTheme {
+        PostCard(modifier = Modifier, image = R.drawable.images_1)
+    }
+}
