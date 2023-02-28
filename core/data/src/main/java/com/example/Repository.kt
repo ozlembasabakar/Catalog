27a6f4f4693227371ebd1bd27e08eb2967d1492d
Ozlem Basabakar <ozlembasabakar@gmail.com>
2023-02-28 11:29:23 +0300

    added local data source to repository

--- core/data/src/main/java/com/example/Repository.kt
+++ core/data/src/main/java/com/example/Repository.kt
@@ -5,13 +5,25 @@ import javax.inject.Inject
 
 class Repository @Inject constructor(
     private val networkDatasource: NetworkDatasource,
+    private val localDataSource: LocalDataSource
 ) {
 
-    suspend fun getImages(): Post {
-        return Post()
+    suspend fun getImages() {
+        saveToDb(
+            //networkDatasource.getImages() ya da networkDatasource.getCategories()
+        )
+        return localDataSource.getAllFromDatabase()
     }
 
     suspend fun getCategories(): Post {
         return Post()
+    }
+
+    fun getFromDatabase() {
+        localDataSource.getAllFromDatabase()
+    }
+
+    private fun saveToDb() {
+        localDataSource.saveToDb()
     }
 }
