20fb1bd70447f018a2f8372795c3a75ee340ba43
Ozlem Basabakar <ozlembasabakar@gmail.com>
2023-03-12 16:11:27 +0300

    added to check internet and show in toast message

--- app/src/main/java/com/example/pinterestclone/homeScreen/HomeScreen.kt
+++ app/src/main/java/com/example/pinterestclone/homeScreen/HomeScreen.kt
@@ -1,7 +1,12 @@
 package com.example.pinterestclone.homeScreen
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.res.Configuration
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import android.widget.Toast
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
@@ -10,6 +15,8 @@ import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.PostCard
 import com.example.model.Category
@@ -32,6 +39,15 @@ fun HomeScreen(
     onRefresh: () -> Unit,
     isRefreshing: Boolean,
 ) {
+
+    val context= LocalContext.current
+
+    if (!checkForInternet(context)) {
+        Toast
+            .makeText(context, stringResource(R.string.toastMessage), Toast.LENGTH_LONG)
+            .show()
+    }
+
     Scaffold(
         //bottomBar = {
         //BottomBar(modifier = modifier)
@@ -73,6 +89,30 @@ fun HomeScreen(
             }
         }
     )
+}
+
+private fun checkForInternet(context: Context): Boolean {
+
+    val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+        val network = connectivityManager.activeNetwork ?: return false
+
+        val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+
+        return when {
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            else -> false
+        }
+    } else {
+        @Suppress("DEPRECATION") val networkInfo =
+            connectivityManager.activeNetworkInfo ?: return false
+        @Suppress("DEPRECATION")
+        return networkInfo.isConnected
+    }
 }
 
 @Preview(name = "LightMode", showSystemUi = true)
