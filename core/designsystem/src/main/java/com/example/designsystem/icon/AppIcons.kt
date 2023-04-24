e3e5c2f0acb6b0c90b90a9e34f46bf2583725acc
Ozlem Basabakar <ozlembasabakar@gmail.com>
2023-04-24 16:39:56 +0300

    added :core:designsystem  module

---
+++ core/designsystem/src/main/java/com/example/designsystem/icon/AppIcons.kt
@@ -0,0 +1,23 @@
+package com.example.designsystem.icon
+
+import com.example.designsystem.R
+
+object AppIcons {
+
+    val SwipeRefreshLoadingArrow = R.drawable.arrows
+
+    val HomeIcon = R.drawable.home_icon
+    val SearchIcon = R.drawable.search_icon
+    val ProfileIcon = R.drawable.profile_icon
+
+    val PseudoImageOne = R.drawable.images_1
+    val PseudoImageTwo = R.drawable.images_2
+    val PseudoImageThree = R.drawable.images_3
+    val PseudoImageFour = R.drawable.images_4
+    val PseudoImageFive = R.drawable.images_5
+
+    val HeartIcon = R.drawable.heart
+    val MoreIcon = R.drawable.more_icon
+
+    val PlaceholderIcon = R.drawable.placeholder
+}
